9ec595b2caf562cce274b12b87725fe0fea0489d
Agunbiade Adedeji <dejongbaba@gmail.com>
2025-02-10 21:43:49 +0100

    chore: update request time

--- src/services/cerbos.ts
+++ src/services/cerbos.ts
@@ -2,11 +2,13 @@ import axios from 'axios';
 
 const checkAccess = async (user, resource, action) => {
     console.log('called this')
+    const currentTime = new Date().toISOString(); // Send ISO timestamp (UTC)
     try {
         const response = await axios.post('http://localhost:3592/api/check/resources', {
             principal: {
                 id: user.id,
                 roles: user.roles,
+                requestTime: currentTime, // Include user's current time
             },
             resources:[
                 {actions: [action],
